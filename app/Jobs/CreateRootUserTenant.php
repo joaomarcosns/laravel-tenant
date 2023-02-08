5f4c8c82f04bcced94042aa9965cc0dfa875d023
João Marcos Neves da Silva <joao.silva@kikker.com.br>
2023-02-08 10:48:51 -0300

    20. Tenant como Root em seu BD

---
+++ app/Jobs/CreateRootUserTenant.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class CreateRootUserTenant implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct(private $tenant)
+    {
+        $this->tenant = $tenant;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $this->tenant->run(function($tenant){
+            User::query()->create($tenant->only('name', 'email', 'password', 'role'));
+        });
+    }
+}
