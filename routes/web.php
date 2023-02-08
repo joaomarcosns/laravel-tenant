d9aefc19aa6ef3a23b06b1b80653458f0c277ee0
João Marcos Neves da Silva <joao.silva@kikker.com.br>
2023-02-08 09:38:36 -0300

    18. Cadastro para nosso Tenant

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\RegisterTenantController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -22,9 +23,8 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+Route::controller(RegisterTenantController::class)->name('tenant.')->group(function () {
+    Route::get('cadastro', 'register')->name('form');
+    Route::post('cadastro', 'store')->name('register');
 });
 
